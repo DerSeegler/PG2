0fd9e9558b4a7b7d0272314a717c236b89dfce83
Yannik S. <yannikseegel@aol.com>
2013-06-05 22:40:29 +0200

    Moved method code into cpp

--- 4/1/myVector.cpp
+++ 4/1/myVector.cpp
@@ -7,11 +7,60 @@
 
 #include "myVector.h"
 
-myVector::myVector() {
+myVector::myVector() : sz(0), elem(0) {
 }
 
-myVector::myVector(const myVector& orig) {
+myVector::myVector(int s) : sz(s), elem(new double[s]) {
+    for (int i = 0; i < s; ++i) {
+        elem[i] = 0.0;
+    }
 }
 
 myVector::~myVector() {
+    delete[] elem;
+    elem = 0;
+}
+
+double& myVector::operator[](int index) {
+    if (index < 0 || sz <= index) throw std::runtime_error("myVector::operator[](), bad index");
+    return elem[index];
+}
+
+int myVector::size() const {
+    return sz;
+}
+
+double* myVector::eleme() const {
+    return elem;
+}
+
+myVec::myVec() : myVector(), lb(0) {
+}
+
+myVec::myVec(int low, int high) : myVector(high - low + 1), lb(low) {
+}
+
+double& myVec::operator[](int i) {
+    if (i < lb || hi() <= i) {
+        throw std::runtime_error("myVector::operator[](), bad index");
+    }
+    
+    return myVector::operator[](i - lb);
+}
+
+double& myVec::operator[](int i) const {
+    if (i < lb || hi() <= i) {
+        throw std::runtime_error("myVector::operator[](), bad index");
+    }
+    
+    double d = myVector::operator[](i - lb);
+    return d;
+}
+
+int myVec::lo() const {
+    return lb;
+}
+
+int myVec::hi() const {
+    return lb + myVector::size() - 1;
 }
--- 4/1/myVector.h
+++ 4/1/myVector.h
@@ -13,62 +13,24 @@ class myVector {
     int sz;
     double* elem;
 public:
-
-    myVector() : sz(0), elem(0) {
-    }
-
-    explicit myVector(int s) : sz(s), elem(new double[s]) {
-        for (int i = 0; i < s; ++i) elem[i] = 0.0;
-    }
-
-    ~myVector() {
-        delete[] elem;
-        elem = 0;
-    }
-    
-    int size() const {
-        return sz;
-    }
-    
-    double* eleme() const {
-        return elem;
-    }
-    
+    myVector();
+    explicit myVector(int s);
+    ~myVector();
+    int size() const;
+    double* eleme() const;
     double& operator[](int);
-};
-
-double& myVector::operator[](int index) {
-    if (index < 0 || sz <= index) throw std::runtime_error("myVector::operator[](), bad index");
-    return elem[index];
 };
 
 class myVec : public myVector {
 public:
-
-    myVec() : myVector(), lb(0) {
-    }
-
-    myVec(int low, int high) : myVector(high - low + 1), lb(low) {
-    }
-
-    double& operator[](int i) {
-        return myVector::operator[](i - lb);
-    }
-
-    int lo() const {
-        return lb;
-    }
-
-    int hi() const {
-        return lb + size() - 1;
-    }
+    myVec();
+    myVec(int, int);
+    double& operator[](int i);
+    double& operator[](int i) const;
+    int lo() const;
+    int hi() const;
 private:
     int lb;
-};
-
-double& myVec::operator[](int index) {
-    if (index < lb || hi() <= index) throw std::runtime_error("myVector::operator[](), bad index");
-    return eleme()[index];
 };
 
 #endif	/* MYVECTOR_H */
